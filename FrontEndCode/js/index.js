0b33276294d73e9abfd365170566b735f5515550
jsliang <1741020489@qq.com>
2018-12-18 14:32:00 +0800

    Node 前端

--- FrontEndCode/js/index.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-  sessionStorage.setItem("id", "1");
-  sessionStorage.setItem("userName", "jsliang");
-  console.log(sessionStorage.getItem("id"));
-  console.log(sessionStorage.getItem("userName"));
-})
