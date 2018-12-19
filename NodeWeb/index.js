e011ba6364f0bc666d157c7d0e95657cc052cf24
jsliang <1741020489@qq.com>
2018-12-19 13:43:43 +0800

    Node 实战

--- NodeWeb/index.js
+++ NodeWeb/index.js
@@ -1,14 +1,8 @@
-// import/export
-const tools = require("jsliang-node/tools");
-
 // 引入 http 模块：http 是提供 Web 服务的基础
 const http = require("http");
 
 // 引入 url 模块：url 是对用户提交的路径进行解析
 const url = require("url");
-
-// 引入 fs 模块：fs 是对文件进行操作
-const fs = require("fs");
 
 // 引入 qs 模块：qs 是对路径进行 json 化或者将 json 转换为 string 路径
 const qs = require("querystring");
@@ -28,76 +22,76 @@ http.createServer(function (req, res) {
   res.setHeader('Content-Type', 'application/json');
 
   if(req.method == "POST") {
-    console.log("\n进入 POST 方法");
-
-    // 接收发送过来的参数
-    let result = "";
-    // 获取前端发来的路由地址
-    let pathName = url.parse(req.url).pathName;
     
+    console.log("\n【POST 形式】");
+
+    // 获取前端发来的路由地址
+    let pathName = req.url;
+    
+    console.log("\n接口为：" + pathName);
+    
+    // 接收发送过来的参数
+    let tempResult = "";
+
     // 数据接入中
     req.addListener("data", function(chunk) {
-      console.log("data：");
-      console.log(chunk);
-      result += chunk;
+      tempResult += chunk;
     });
 
     // 数据接收完成
     req.addListener("end", function() {
-      console.log("qs 拆分：");
-      console.log(qs.parse(result));
+      
+      let reuslt = JSON.stringify(qs.parse(tempResult));
+      console.log("\n参数为：");
+      console.log(reuslt);
+
+      if(pathName == "/sendMessage") {
+        
+        console.log("\n【API - 提交留言信息】");
+
+      } else if (pathName == "/login") {
+
+        console.log("\n【API - 登录】");
+
+      } else if (pathName == "/register") {
+
+        console.log("\n【API - 注册】");
+
+      }
 
       // 返回数据
-      res.write(result);
+      res.write(reuslt);
       
       // 结束响应
       res.end();
     })
 
   } else if(req.method == "GET") {
-    console.log("\n进入 GET 方法");
+    
+    console.log("\n【GET 形式】");
 
-    // 解析 url 参数部分
-    let params = url.parse(req.url, true).query;
-    console.log("params：");
-    console.log(params);
+    // 解析 url 接口
+    let pathName = url.parse(req.url).pathname;
 
-    // 结束响应
-    res.end();
+    console.log("\n接口为：" + pathName);
+
+    if(pathName == "/getMessage") {
+
+      console.log("\n【API - 获取留言信息】");
+
+      // 解析 url 参数部分
+      let params = url.parse(req.url, true).query;
+          
+      console.log("\n参数为：");
+      console.log(params);
+
+      // 返回数据
+      res.write(JSON.stringify(params));
+
+      // 结束响应
+      res.end();
+    }
+
   }
-
-
-
-  // // 防止 url 请求 2 次
-  // if(req.url != "/favicon.ico") {
-    
-  //   console.log("开始编程");
-
-  //   // 判断 url
-  //   console.log(req.url);
-
-  //   // 引用工具库
-  //   console.log(tools.add(1, 2, 3));
-    
-  //   // 引用 fs
-  //   fs.readFile("index.js", (err) => {
-  //     if(err) {
-  //       console.log(err);
-  //     } else {
-  //       console.log("读取 index.js 成功！");
-  //     }
-  //   })
-
-  // }
-
-  // // 设置 HTTP 头部，状态码是 200，文件类型是 html，字符集是 utf8
-  // res.writeHead(200, {
-  //   "Content-Type": "text/html;charset=UTF-8"
-  // });
-
-  // // 往页面打印值
-  // res.write('<h1 style="text-align:center">Hello NodeJS</h1>');
-
-  
 
 }).listen(8888); // 监听的端口
