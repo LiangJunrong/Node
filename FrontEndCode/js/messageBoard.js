9032aefa52ce4a40e3ad32419f6feebeb97d4ead
jsliang <1741020489@qq.com>
2018-12-20 11:52:01 +0800

    Node 页面

--- FrontEndCode/js/messageBoard.js
+++ FrontEndCode/js/messageBoard.js
@@ -1,10 +1,9 @@
 $(function() {
   let userName = sessionStorage.getItem("userName");
   let userId = sessionStorage.getItem("id")
-  if(userName && userId) {
-    sessionStorage.setItem("id", "1");
-    sessionStorage.setItem("userName", "jsliang");
-  } else {
-    // window.location.href = "../login.html";
+  if(userName && userId) { // 如果没有存储
+    
+  } else { // 如果有存储
+    window.location.href = "../login.html";
   }
 })
