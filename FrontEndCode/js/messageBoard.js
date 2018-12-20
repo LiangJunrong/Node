a8b9fb5b73e9487cd09f042563f24ba359009724
jsliang <1741020489@qq.com>
2018-12-20 13:49:04 +0800

    Node 前端代码

--- FrontEndCode/js/messageBoard.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-  let userName = sessionStorage.getItem("userName");
-  let userId = sessionStorage.getItem("id")
-  if(userName && userId) { // 如果没有存储
-    
-  } else { // 如果有存储
-    window.location.href = "../login.html";
-  }
-})
